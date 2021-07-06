2233031dcf6d409ca4f55ceb3c10654f940cd0d2
Galib792 <85933934+Galib792@users.noreply.github.com>
2021-07-06 14:16:13 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
+please join our channel before proceeding: https://t.me/Tips_Tik
 Thank you for using me 😬
 Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
